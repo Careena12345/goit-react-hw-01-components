929d00733eaaa73443b6ab59f6a5037b9e0ce782
Careena12345 <45653779+Careena12345@users.noreply.github.com>
2020-03-06 21:45:04 +0200

    Updates

---
+++ precache-manifest.09513c963199f834fb9c1311d3a2c328.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b4d5792fb7355aefd775e241b6a4489a",
+    "url": "/goit-react-hw-01-components/index.html"
+  },
+  {
+    "revision": "eb51ce4f0bf964bf4490",
+    "url": "/goit-react-hw-01-components/static/css/main.ea64334a.chunk.css"
+  },
+  {
+    "revision": "ee240cebaa5cf2d8c66b",
+    "url": "/goit-react-hw-01-components/static/js/2.20179117.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/goit-react-hw-01-components/static/js/2.20179117.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eb51ce4f0bf964bf4490",
+    "url": "/goit-react-hw-01-components/static/js/main.8d67b790.chunk.js"
+  },
+  {
+    "revision": "a62329f2cc0b24bc37f9",
+    "url": "/goit-react-hw-01-components/static/js/runtime-main.39b5137b.js"
+  }
+]);
